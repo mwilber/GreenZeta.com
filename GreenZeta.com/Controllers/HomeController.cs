b86452d6d949ee8dde08dd7dbbec03855a3532b9
Matthew Wilber <mwilber@gmail.com>
2012-09-11 20:02:13 -0700

    add placeholders for main site
    index, listing & profile pages in home controller. Simple placeholder
    data queries.

--- GreenZeta.com/Controllers/HomeController.cs
+++ GreenZeta.com/Controllers/HomeController.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GreenZeta.com.Models;
 
 namespace GreenZeta.com.Controllers
 {
     public class HomeController : Controller
     {
+        private ProjectDBContext db = new ProjectDBContext();
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Home Page";
 
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Listing(string id)
         {
-            return View();
+            var projects = from m in db.Projects select m;
+
+            if (!String.IsNullOrEmpty(id))
+            {
+                projects = projects.Where(s => s.alias.Equals(id));
+            }
+
+            return View(projects);
+        }
+
+        public ActionResult Profile(string id)
+        {
+            var projects = from m in db.Projects select m;
+
+            if (!String.IsNullOrEmpty(id))
+            {
+                projects = projects.Where(s => s.alias == id);
+            }
+
+            return View(projects);
         }
     }
 }
