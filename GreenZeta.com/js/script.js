bcc567c89a1a3dc792be1f7a109125691ec13d98
Matthew Wilber <mwilber@gmail.com>
2012-09-19 22:37:25 -0400

    Graphic changes to backgrounds
    - Add grass background to content area
    - Adjust positioning of items in header
    - Add touch events to header

--- GreenZeta.com/js/script.js
+++ GreenZeta.com/js/script.js
@@ -46,37 +46,33 @@ $(document).ready( function($){
         arrowsNavHideOnTouch: true,
         keyboardNavEnabled: true
     });
-	
-	$('#hga').mouseenter( function () {
-	    $('#header #stars').css('backgroundPosition', '-40px');
-	    $('#header #grass').css('backgroundPosition', '-30px');
-	    $('#header #logo').css('backgroundPosition', '-30px');
-	    $('#header #hga').css('backgroundPosition', '-60px');
-	    $('#header #hgb').css('backgroundPosition', '-60px');
-	}).mouseleave( function () {
-		$('#header #stars').css('backgroundPosition', '-50px');
-		$('#header #grass').css('backgroundPosition', '-50px');
-		$('#header #logo').css('backgroundPosition', '-50px');
-		$('#header #hga').css('backgroundPosition', '-50px');
-		$('#header #hgb').css('backgroundPosition', '-50px');
-	});
 
-$('#hgb').mouseenter(function () {
-        $('#header #stars').css('backgroundPosition', '-60px');
-        $('#header #grass').css('backgroundPosition', '-70px');
-        $('#header #logo').css('backgroundPosition', '-70px');
-        $('#header #hga').css('backgroundPosition', '-40px');
-        $('#header #hgb').css('backgroundPosition', '-40px');
-	}).mouseleave( function () {
-		$('#header #stars').css('backgroundPosition', '-50px');
-		$('#header #grass').css('backgroundPosition', '-50px');
-		$('#header #logo').css('backgroundPosition', '-50px');
-		$('#header #hga').css('backgroundPosition', '-50px');
-		$('#header #hgb').css('backgroundPosition', '-50px');
-	});
+    $('#hga').mouseenter(HeadLeft).mouseleave(HeadReset).bind('touchstart', HeadLeft).bind('touchend', HeadReset);
+
+    $('#hgb').mouseenter(HeadRight).mouseleave(HeadReset).bind('touchstart', HeadRight).bind('touchend', HeadReset);
 	
 } );
 
+function HeadReset() {
+    $('#header #stars').css('backgroundPosition', '-50px');
+    $('#header #grass').css('backgroundPosition', '-50px');
+    $('#header #logo').css('backgroundPosition', '-50px');
+    $('#header #hga').css('backgroundPosition', '-20px');
+    $('#header #hgb').css('backgroundPosition', '-150px');
+}
 
+function HeadLeft() {
+    $('#header #stars').css('backgroundPosition', '-40px');
+    $('#header #grass').css('backgroundPosition', '-30px');
+    $('#header #logo').css('backgroundPosition', '-30px');
+    $('#header #hga').css('backgroundPosition', '-40px');
+    $('#header #hgb').css('backgroundPosition', '-170px');
+}
 
-
+function HeadRight() {
+    $('#header #stars').css('backgroundPosition', '-60px');
+    $('#header #grass').css('backgroundPosition', '-70px');
+    $('#header #logo').css('backgroundPosition', '-70px');
+    $('#header #hga').css('backgroundPosition', '0px');
+    $('#header #hgb').css('backgroundPosition', '-130px');
+}
